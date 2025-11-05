3d7851046d7c93dab9c5388244cafcba4f1f4b82
SumaiyaNishat <sumaiyanishat94@gmail.com>
2025-11-05 11:49:44 +0600

    Add project files.

---
+++ WebFromPractice/Test.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="WebFromPractice.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Name:<asp:TextBox ID="Name" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </p>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Button" />
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Button" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebFromPractice/Test.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFromPractice
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var x = 100;
+
+            if (!IsPostBack)
+            {
+                ViewState["count"] = Convert.ToString(x);
+                ViewState["SecondCount"] = Convert.ToString(x);
+                Label5.Text = Convert.ToString(x);
+                Label6.Text = Convert.ToString(x);
+
+            }
+
+
+            if (!IsPostBack)
+            {
+                Label3.Text = "100";
+                Label4.Text = "100";
+            }
+
+
+            if (!IsPostBack)
+            {
+                Label2.Text = "0";
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var res = Name.Text;
+            Label1.Text = "Hello, " + res + "!";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var count = Convert.ToInt32(Label2.Text);
+            count++;
+            Label2.Text = Convert.ToString(count);
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var count = Convert.ToInt32(Label4.Text);
+            count--;
+            Label4.Text = Convert.ToString(count);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            var count = Convert.ToInt32(ViewState["count"]);
+            count++;
+            ViewState["count"] = Convert.ToString(count);
+            Label5.Text = Convert.ToString(count);
+        }
+    }
+}
